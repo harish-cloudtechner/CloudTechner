eb1e3e8efe2b6a6337e130c7fa92e8ac24d7e51e
hsct2707 <76257295+hsct2707@users.noreply.github.com>
2021-02-19 16:34:35 +0530

    Update vpc.tf

--- vpc.tf
+++ vpc.tf
@@ -331,5 +331,5 @@ resource "aws_db_instance" "dbinst" {
   name                 = "mydb"
   username             = "harish"
   password             = "harish2707"
-  parameter_group_name = "default.mysql5.7"
+  db_subnet_group_name = aws_db_subnet_group.dbgroup.id
 }
