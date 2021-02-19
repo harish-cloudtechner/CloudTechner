5991c0c2f42e08496d0aa58c7b8e65bc6230b5bf
hsct2707 <76257295+hsct2707@users.noreply.github.com>
2021-02-19 17:30:33 +0530

    Update vpc.tf

--- vpc.tf
+++ vpc.tf
@@ -336,7 +336,7 @@ resource "aws_db_instance" "dbinst" {
   db_subnet_group_name = aws_db_subnet_group.dbgroup.id
 }
 resource "aws_s3_bucket" "b" {
-  bucket = "terraform_buck"
+  bucket = "terraformbuck"
   acl    = "private"
 
   tags = {
